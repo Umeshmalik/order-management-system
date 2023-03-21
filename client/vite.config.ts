8619e72c7364df1daa17026c647645950adda435
Umesh-malik-byjus <Lucky.umeshmalik@gmail.com>
2023-03-21 15:42:16 +0530

    removed tc

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom'
-  },
   server:{
     port: 80
   },
